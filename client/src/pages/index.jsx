2fe6d79ddff4c7981ba99b0686d243fbc16510a6
DishantWakalkar <dishantwakalkar@gmail.com>
2022-10-09 23:42:46 +0530

    Revert "dashboard"
    This reverts commit b744364798b8b133ad44d7adb47df883ae4fd3ca.

--- client/src/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-export { default as Overall } from './Overall';
-export { default as ColorPicker } from './ColorPicker';
-export { default as Area } from './Charts/Area';
-export { default as Bar } from './Charts/Bar';
-export { default as ColorMapping } from './Charts/ColorMapping';
-export { default as Line } from './Charts/Line';
-export { default as Pie } from './Charts/Pie';
-export { default as Pyramid } from './Charts/Pyramid';
-export { default as Stacked } from './Charts/Stacked';
